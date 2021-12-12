a6b20f874c6505ce51205f1e5db9a2b93a8bd3e5
chrispit <chrispit@ou.edu>
2021-12-12 17:35:11 -0500

    styling

---
+++ js/quote.js
@@ -0,0 +1,11 @@
+const quote = document.querySelector('.quote');
+
+const q = [
+    "\'I wish I were a bird\'",
+    "\'damn, she just like me fr fr\'",
+    "\'Flock off feather-face\'"
+];
+
+let num = Math.floor(Math.random() * 3);
+
+quote.innerHTML = `${q[num]}`;
